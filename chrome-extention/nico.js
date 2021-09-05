559f34cee76401dd3c059007a37c6f878fe12fa8
oska1204 <oska1204@edu.campusvejle.dk>
2021-09-05 18:02:46 +0200

    Fixes extension bug

--- chrome-extention/nico.js
+++ chrome-extention/nico.js
@@ -2,27 +2,34 @@ setTimeout(() => {
     document.querySelector('.unplayed div').click()
 }, 1000)
 
-setTimeout(() => {
-const v = document.querySelector('video')
-let date
-const next = () => {
-    chrome.runtime.sendMessage(
-        { contentScriptQuery: 'ended' },
-        status => { }
-    )
+const realVideo = setInterval(()=>{
+    if (document.querySelector('video').hasAttribute('src')) {
+        start()
+        clearInterval(realVideo)
+    }
+}, 100)
+
+function start() {
+    const v = document.querySelector('video')
+    let date
+    const next = () => {
+        chrome.runtime.sendMessage(
+            { contentScriptQuery: 'ended' },
+            status => { }
+        )
+    }
+
+    const playFn = () => {
+        date = Date.now()
+        v.removeEventListener('play', playFn)
+    }
+    v.addEventListener('play', playFn)
+
+    v.addEventListener('timeupdate', e => {
+        const amount = (v.duration * 1000 + date) - Date.now()
+        if (amount < 0)
+            next()
+    })
+
+    v.addEventListener('ended', next)
 }
-
-const playFn = () => {
-    date = Date.now()
-    v.removeEventListener('play', playFn)
-}
-v.addEventListener('play', playFn)
-
-v.addEventListener('timeupdate', e => {
-    const amount = (v.duration * 1000 + date) - Date.now()
-    if (amount < 0)
-        next()
-})
-
-v.addEventListener('ended', next)
-}, 3000)
